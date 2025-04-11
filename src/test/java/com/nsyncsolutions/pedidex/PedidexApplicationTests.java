11670f901a6e5f0f81488a6e72dc78a11ff99beb
edmilson.dias@mozaiko.io <eddiasdev@gmail.com>
2025-04-11 15:42:55 -0300

    Initialize Spring Boot project with Gradle setup and PostgreSQL support

---
+++ src/test/java/com/nsyncsolutions/pedidex/PedidexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsyncsolutions.pedidex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
